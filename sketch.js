cd066c4cc9e26a86f3ebfd843690e208ef095b9a
Athena <az2679@nyu.edu>
2023-03-06 01:33:24 -0500

    dots

--- sketch.js
+++ sketch.js
@@ -27,26 +27,55 @@ function draw() {
   let x2 = sin(t / 20) * amplitude * 50;
   let y2 = cos(t / 10) * amplitude * 20;
   
-  strokeWeight(0.5);
-  noFill()
-  stroke(200, 150)
-  // ellipse(x1, y1, 15)
-
-
-  rotate(t*0.5)
-  // for (let i=-x1; i<x1; i++){
-  //   for(let j=-y1; j<y1; j++){
-  //     point(i, j)
-  //   }
+  // if (y1 < height/4 || y1 > (height/4) *3 ){
+  //   strokeWeight(5);
+  // } else {
+  //   strokeWeight(0.5);
   // }
 
-  // rect(x1, y1, x2, y2)
+  
+  strokeWeight(5)
+  noFill()
+  stroke(200, 200)
+  point(x1*10, y1*10)
+  point(-x1*10, y1*10)
+  point(x1*10, -y1*10)
+  point(-x1*10, -y1*10)
+
+  point(x2*0.01, y1*10)
+  point(-x2*0.01, y1*10)
+  point(x2*0.01, -y1*10)
+  point(-x2*0.01, -y1*10)
+
+  point(x1*10, y2*0.01)
+  point(-x1*10, y2*0.01)
+  point(x1*10, -y2*0.01)
+  point(-x1*10, -y2*0.01)
+
+  point(x2*0.01, y2*0.01)
+  point(-x2*0.01, y2*0.01)
+  point(x2*0.01, -y2*0.01)
+  point(-x2*0.01, -y2*0.01)
+
+  point(y1*10, x1*10)
+  point(-y1*10, -x1*10)
+  point(y1*10, x2*0.01)
+  point(-y1*10, -x2*0.01)
+  point(y2*0.01, x1*10)
+  point(-y2*0.01, -x1*10)
+  point(y2*0.01, x2*0.01)
+  point(-y2*0.01, -x2*0.01)
+
+
+
+console.log (y1)
+  rotate(t*0.5)
   
   
   strokeWeight(2);
   stroke(200, 200);
   line(x1, y1, x2*0.005, y2*0.005);
-  t += 0.5;
+  t += 0.4;
 }
 
 function windowResized() {
